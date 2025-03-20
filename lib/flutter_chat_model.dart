4ee855c55a8e4508fc37f878ea5eba4e0ddcd7f6
taburity <thais.burity@ufape.edu.br>
2025-03-20 10:43:05 -0300

    Organização dos comentários.

--- lib/flutter_chat_model.dart
+++ lib/flutter_chat_model.dart
@@ -1,32 +1,20 @@
-import "dart:io";
 import "package:flutter/material.dart";
 
 
 class FlutterChatModel extends ChangeNotifier {
 
   String greeting = "";
-
   String userName = "";
-
-  //Texto que é exibido quando o usuário não está em nenhuma sala
   static final String DEFAULT_ROOM_NAME = "Not currently in a room";
-
   String currentRoomName = DEFAULT_ROOM_NAME;
-
   List currentRoomUserList = [ ];
-
-  /// Is the Current Room option in the drawer enabled? (only when in a room)
+  // Indica se a opção Current Room está habilitada no drawer
   bool currentRoomEnabled = false;
-
-  /// Os elementos são um mapa na forma { userName : "", message : "" }.
+  // Os elementos são um mapa na forma { userName : "", message : "" }.
   List currentRoomMessages = [ ];
-
   List roomList = [ ];
-
   List userList = [ ];
-
   bool creatorFunctionsEnabled = false;
-
   Map roomInvites = { };
 
   void setGreeting(final String inGreeting) {
@@ -68,7 +56,6 @@ class FlutterChatModel extends ChangeNotifier {
   //O mapa de entrada descreve cada sala com um par chave-valor. Queremos uma lista só com o valor.
   void setRoomList(final Map inRoomList) {
     print("## FlutterChatModel.setRoomList(): inRoomList = $inRoomList");
-
     List rooms = [ ];
     for (String roomName in inRoomList.keys) {
       Map room = inRoomList[roomName];
@@ -104,22 +91,22 @@ class FlutterChatModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  /// Um convite resulta em um Snackbar exibido por alguns segundos. Depois que ele desaparece, é necessário
-  /// saber se o usuário pode entrar numa sala, então aqui o mapa tem como chave o nome da sala e o valor é um boolean.
-  /// Se o boolean é true, tem convite, então o usuário pode entrar.
-  /// Não precisa de notificação porque não há uma reconstrução de widgets
+  // Um convite resulta em um Snackbar exibido por alguns segundos. Depois que ele desaparece, é necessário
+  // saber se o usuário pode entrar numa sala, então aqui o mapa tem como chave o nome da sala e o valor é um boolean.
+  // Se o boolean é true, tem convite, então o usuário pode entrar.
+  // Não precisa de notificação porque não há uma reconstrução de widgets
   void addRoomInvite(final String inRoomName) {
     print("## FlutterChatModel.addRoomInvite(): inRoomName = $inRoomName");
     roomInvites[inRoomName] = true;
   }
 
-  /// Não precisa de notificação porque não há uma reconstrução de widgets
+  // Não precisa de notificação porque não há reconstrução de widgets
   void removeRoomInvite(final String inRoomName) {
     print("## FlutterChatModel.removeRoomInvite(): inRoomName = $inRoomName");
     roomInvites.remove(inRoomName);
   }
 
-  /// Não precisa de notificação porque não há uma reconstrução de widgets
+  // Não precisa de notificação porque não há reconstrução de widgets
   void clearCurrentRoomMessages() {
     print("## FlutterChatModel.clearCurrentRoomMessages()");
     currentRoomMessages = [ ];
