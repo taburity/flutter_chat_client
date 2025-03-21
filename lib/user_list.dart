cb5743f31e9d8c8b31a3cdb01a7a219eab1038f4
taburity <thais.burity@ufape.edu.br>
2025-03-20 22:06:53 -0300

    Remoção de uso do console.

--- lib/user_list.dart
+++ lib/user_list.dart
@@ -8,10 +8,8 @@ class UserList extends StatelessWidget {
   @override
   Widget build(final BuildContext inContext) {
     print("## UserList.build()");
-
     return Consumer<FlutterChatModel>(
       builder: (BuildContext inContext, FlutterChatModel inModel, Widget? inChild) {
-        print("users: " + (inModel.userList.length).toString());
         return Scaffold(
           appBar : AppBar(title : Text("User List")),
           drawer : AppDrawer(),
@@ -20,7 +18,6 @@ class UserList extends StatelessWidget {
             gridDelegate : SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount : 3),
             itemBuilder : (BuildContext inContext, int inIndex) {
               Map user = inModel.userList[inIndex];
-              print(user);
               return Padding(padding : EdgeInsets.fromLTRB(10, 10, 10, 10), child : Card(
                 child : Padding(padding : EdgeInsets.fromLTRB(10, 10, 10, 10),
                   child : GridTile(
